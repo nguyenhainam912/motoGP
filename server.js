585e3380d44efc7ceb9b0f71140400057804b22f
law nguyen <dognam912@gmail.com>
2024-09-20 15:40:29 +0700

    nam hack 20/9

--- server.js
+++ server.js
@@ -23,6 +23,13 @@ mongoose.connect(process.env.DBURL).then(() => {
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
+
 app.use("/api/riders", RiderRoute)
 app.use("/api/teams", TeamRoute)
 app.use("/api/countries", CountryRoute)
