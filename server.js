3c4165fbc1a1c56e890fc8aab20e1d623dcc5bbc
law nguyen <dognam912@gmail.com>
2024-09-30 19:32:00 +0700

    30/9

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({extended: true}))
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,PATCH');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 })
